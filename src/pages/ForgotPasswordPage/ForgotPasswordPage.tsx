3760081dafa712b221498d8ad362012eca0873dd
Fathulla Rashidov <fathullarasidov239@gmail.com>
2023-09-14 14:01:22 +0500

    trying to add hook-forms

--- src/pages/ForgotPasswordPage/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage/ForgotPasswordPage.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { Controller } from "react-hook-form";
 import { AppHeader } from "../../components/UI/AppHeader/AppHeader";
 import { AppInput } from "../../components/UI/AppInput/AppInput";
 import { Container } from "../../components/UI/Container/Container";
 import { AppButton } from "./../../components/UI/AppButton/AppButton";
+import * as yup from "yup"
+
 
 
 interface ForgotPassword {
-    
+  userphone: string;
 }
+
+const forgotPasswordShcema = yup.object ({
+  userphone: yup.string().required("Объязательное поле")
+})
+
 
 const ForgotPasswordPage = () => {
   return (
